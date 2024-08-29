ccfceb55548545761c3c401c764c893fb5eb8611
Daniel Crenna <daniel.crenna@gmail.com>
2024-08-29 17:52:04 -0400

    add more factory defaults

--- blazor/twittericons/IconFactory.cs
+++ blazor/twittericons/IconFactory.cs
@@ -3,6 +3,8 @@
 public static class IconFactory
 {
     public static IconType DefaultIconType = IconType.QuestionCircle;
+    public static IconStyle DefaultIconStyle = IconStyle.Outline;
+
     public static Type DefaultIcon = typeof(Outline.QuestionCircleIcon);
 
     public static Type GetIconType(IconType type, IconStyle style)
