8bb55073fd1ce755039f36ef5f3836bb84ba6674
Daniel Crenna <daniel.crenna@gmail.com>
2024-08-29 17:27:29 -0400

    switch to factory pattern to make adjustments easier

---
+++ blazor/twittericons/IconFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace twittericons;
+
+public static class IconFactory
+{
+    public static IconType DefaultIconType = IconType.QuestionCircle;
+    public static Type DefaultIcon = typeof(Outline.QuestionCircleIcon);
+
+    public static Type GetIconType(IconType type, IconStyle style)
+    {
+        var typeName = $"{typeof(Icon).Namespace}.{style}.{type}Icon";
+        var iconType = System.Type.GetType(typeName, false, true);
+        return iconType ?? DefaultIcon;
+    }
+}
